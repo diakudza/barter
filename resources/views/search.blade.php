9a058025b62f7c228ca3fe3626e03e88e2d7be1a
Mihail <77148342+diakudza@users.noreply.github.com>
2022-08-05 14:45:55 +0300

    Search (#5)
    * search task
    
    * search task fix

--- resources/views/search.blade.php
+++ resources/views/search.blade.php
@@ -4,25 +4,30 @@
 
 @section('content')
 
-<div class="mt-5 mb-5 h-25">
-</div>
+    <div class="mt-5 mb-5 h-25">
+    </div>
 
-@include('component.searchForm')
+    @include('component.searchForm')
 
-@if (isset($searchResult))
+    @if (isset($searchResult))
 
-<div class="container mt-5">
-    <table class="mt-5 table-bordered">
-        @foreach( $searchResult as $item)
+        <div class="container mt-5">
+            <table class="mt-5 table-bordered">
+                @foreach( $searchResult as $item)
 
-        <tr>
-            <td>{{ $item['id'] }}</td>
-            <td><a href="{{ route('ad.show', $item['id'])}}"> {{ $item['title'] }}</a></td>
-            <td>{{ $item['text'] }}</td>
-            <td>{{ $item->category->title }}</td>
-        </tr>
-        @endforeach
-    </table>
-</div>
-@endif
+                    <tr>
+                        <td>{{ $item['id'] }}</td>
+                        <td><a href="{{ route('ad.show', $item['id'])}}"> {{ $item['title'] }}</a></td>
+                        <td>{{ $item['text'] }}</td>
+                        <td>{{ $item->category->title }}</td>
+                        <td>
+                            @if ($item->status == 4)
+                                <p>в архиве</p>
+                            @endif
+                        </td>
+                    </tr>
+                @endforeach
+            </table>
+        </div>
+    @endif
 @endsection
