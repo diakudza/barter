007d5d16cfdf12f62850b15f9045b9bd6dcb3331
Artem Biryukov <carbon0093@gmail.com>
2022-08-16 17:09:37 +0300

    add: добавил bootstrap, поправил некоторые моменты в верстке

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,5 +1,10 @@
 import "./bootstrap";
 
+//Bootstrap
+import "/node_modules/bootstrap/dist/js/bootstrap.bundle.min.js";
+
+//Модули
 import { headerJS } from "./navbar";
 
+//Вызовы модулей
 headerJS();
