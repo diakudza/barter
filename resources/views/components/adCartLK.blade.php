f6d99b9b0cab3fc3bb4ae03e7494c7697d1eb85d
Artem Biryukov <carbon0093@gmail.com>
2022-08-16 16:35:13 +0300

    fix: конфликты слияния

--- resources/views/components/adCartLK.blade.php
+++ resources/views/components/adCartLK.blade.php
@@ -1,34 +1,26 @@
-
-    <div class="card mb-4 box-shadow bg-light w-25">
-        <img class="card-img-top"
-             data-src="holder.js/100px225?theme=thumb&amp;bg=55595c&amp;fg=eceeef&amp;text=Thumbnail"
-             alt="Thumbnail [100%x225]" style="height: 225px; width: 100%; display: block;"
-             src="
+<div class="card mb-4 box-shadow bg-light w-25">
+    <img class="card-img-top" data-src="holder.js/100px225?theme=thumb&amp;bg=55595c&amp;fg=eceeef&amp;text=Thumbnail" alt="Thumbnail [100%x225]" style="height: 225px; width: 100%; display: block;" src="
                                         @if (count($ad->imageMain)) {{ Storage::url($ad->imageMain[0]->path) }}
-             @else {{ Storage::url('images/clean.webp') }} @endif"
-             data-holder-rendered="true">
-        @if ( $ad->status->title == 'archived')
-            <h1 class="position-absolute start-50 top-50 opacity-50 text-danger">в архиве</h1>
-        @endif
-        <div class="card-body">
-            <p class="card-text">{{ $ad->title }}</p>
-            <p class="card-text">Город подачи: {{ $ad->city->name }}</p>
-            <p class="card-text">Категория: {{ $ad->category->title }}</p>
-            <div class="d-flex justify-content-between align-items-center">
-                <div class="btn-group">
-                    <a href="{{ route('ad.show', $ad->id) }}"
-                       class="btn btn-sm btn-outline-secondary">Просмотреть</a>
-                    <a href="{{ route('user.profile.editAd', ['ad' => $ad->id]) }}"
-                       class="btn btn-sm btn-outline-secondary">Изменить</a>
-                </div>
-            </div>
-            <div class="d-flex flex-column ">
-                <small class="text-muted">Статус: {{ $ad->status->description }}</small>
-                <small class="text-muted">Просмотрели: {{ $ad->show_count }}</small>
-                <small class="text-muted">Добавили в
-                    избранное: {{ count($ad->favoriteUsers) }}</small>
-                <small class="text-muted">Откликнулись: {{ count($ad->usersWished) }}</small>
+             @else {{ Storage::url('images/clean.webp') }} @endif" data-holder-rendered="true">
+    @if ( $ad->status->title == 'archived')
+    <h1 class="position-absolute start-50 top-50 opacity-50 text-danger">в архиве</h1>
+    @endif
+    <div class="card-body">
+        <p class="card-text">{{ $ad->title }}</p>
+        <p class="card-text">Город подачи: {{ $ad->city->name }}</p>
+        <p class="card-text">Категория: {{ $ad->category->title }}</p>
+        <div class="d-flex justify-content-between align-items-center">
+            <div class="btn-group">
+                <a href="{{ route('ad.show', $ad->id) }}" class="btn btn-sm btn-outline-secondary">Просмотреть</a>
+                <a href="{{ route('user.profile.editAd', ['ad' => $ad->id]) }}" class="btn btn-sm btn-outline-secondary">Изменить</a>
             </div>
         </div>
+        <div class="d-flex flex-column ">
+            <small class="text-muted">Статус: {{ $ad->status->description }}</small>
+            <small class="text-muted">Просмотрели: {{ $ad->show_count }}</small>
+            <small class="text-muted">Добавили в
+                избранное: {{ count($ad->favoriteUsers) }}</small>
+            <small class="text-muted">Откликнулись: {{ count($ad->usersWished) }}</small>
+        </div>
     </div>
-
+</div>
--- resources/views/user/profile/editAd.blade.php
+++ resources/views/user/profile/editAd.blade.php
@@ -3,97 +3,18 @@
 @section('title', 'Редактировать объявление')
 
 @section('content')
-    <div class="container">
-        <h3>Страница редактирования объявления</h3>
+<div class="container">
+    <h3>Страница редактирования объявления</h3>
 
-<<<<<<< HEAD
-        <form action="{{ route('ad.update', $ad->id) }}" method="post" enctype="multipart/form-data">
-            @csrf
-            @method('put')
-            <div class="form-group">
-                <label for="title">Название</label>
-                <!-- Здесь конструкци if endif  в одну строку, так как иначе в строку в форме добавляется куча пробелов -->
-                <input type="text" id="title" name="title" placeholder="Название"
-                       value="@if (old('title')) {{ old('title') }}@else{{ $ad->title }} @endif" required>
-            </div>
-            <div>
-                <label for="text">Описание</label>
-                <!-- Здесь конструкци if endif  в одну строку, так как иначе в строку в форме добавляется куча пробелов -->
-                <textarea name="text" id="text" rows="20" required>
-@if (old('text')){{ old('text') }}@else{{ $ad->text }}@endif
-</textarea>
-            </div>
-            <div>
-                <label for="image">Загрузить фото</label>
-                <input type="file" name="image" id="image">
-                <img src="{{ Storage::url($ad->image) }}" alt="image" height=400>
-            </div>
-            <div>
-
-            </div>
-            <div>
-                <label for="category_id">Категория</label>
-                <select name="category_id" id="category_id" required>
-                    @foreach ($categoriesList as $category)
-                        <option value="{{ $category->id }}"
-                                @if (old('category_id')) @if (old('category_id') == $category->id) selected
-                                @endif @endif
-                                @if ($ad->category_id == $category->id) selected @endif>
-                            {{ $category->title }}
-                        </option>
-                    @endforeach
-                </select>
-            </div>
-            <input type="hidden" name="user_id" value="{{ Auth::user()->id }}">
-            <div>
-                <label for="free">Отдам просто так</label>
-                <input type="radio" name="barter_type" id="free" value="free" checked>
-                <!-- Функционал обмена реализуем позже -->
-                <label for="barter">Обмен</label>
-                <input type="radio" name="barter_type" id="barter" value="barter" disabled>
-                <label for="barter_for">Обменяю на</label>
-                <input type="text" name="barter_for" id="barter_for" disabled>
-            </div>
-            <div>
-                <label for="city_id">Город подачи</label>
-                <select name="city_id" id="city_id" required>
-                    @foreach ($citiesList as $city)
-                        <option value="{{ $city->id }}"
-                                @if (old('city_id')) @if (old('city_id') == $city->id) selected @endif @endif
-                                @if ($ad->city_id == $city->id) selected @endif>
-                            {{ $city->name }}
-                        </option>
-                    @endforeach
-                </select>
-            </div>
-            <div>
-                <label for="status_id">Статус</label>
-                <select name="status_id" id="status_id">
-                    @foreach ($statusesList as $status)
-                        <option value="{{ $status->id }}"
-                                @if (old('status_id')) @if (old('status_id') == $status->id)
-                                selected @endif
-                                @endif
-                                @if ($ad->status_id == $status->id) selected @endif>
-                            {{ $status->description }}
-                        </option>
-                    @endforeach
-                </select>
-            </div>
-            <div>
-                <button type="submit">Сохранить</button>
-                <button type="reset">Отменить изменения</button>
-=======
-        <form action="{{ route('ad.update', $ad->id) }}" method="post" enctype="multipart/form-data" class="form-1">
-            @csrf
-            @method('put')
-            <div class="row">
+    <form action="{{ route('ad.update', $ad->id) }}" method="post" enctype="multipart/form-data" class="form-1">
+        @csrf
+        @method('put')
+        <div class="row">
             <div class="w-50">
                 <div class="form-group">
                     <label for="title">Название</label>
                     <!-- Здесь конструкци if endif  в одну строку, так как иначе в строку в форме добавляется куча пробелов -->
-                    <input type="text" id="title" name="title" placeholder="Название" class="form-control"
-                           value="@if (old('title')) {{ old('title') }}@else{{ $ad->title }} @endif" required>
+                    <input type="text" id="title" name="title" placeholder="Название" class="form-control" value="@if (old('title')) {{ old('title') }}@else{{ $ad->title }} @endif" required>
                 </div>
                 <div>
                     <label for="text">Описание</label>
@@ -110,12 +31,11 @@
                     <label for="category_id">Категория</label>
                     <select name="category_id" id="category_id" required class="form-select ">
                         @foreach ($categoriesList as $category)
-                            <option value="{{ $category->id }}"
-                                    @if (old('category_id')) @if (old('category_id') == $category->id) selected @endif
-                                    @endif
-                                    @if ($ad->category_id == $category->id) selected @endif>
-                                {{ $category->title }}
-                            </option>
+                        <option value="{{ $category->id }}" @if (old('category_id')) @if (old('category_id')==$category->id) selected @endif
+                            @endif
+                            @if ($ad->category_id == $category->id) selected @endif>
+                            {{ $category->title }}
+                        </option>
                         @endforeach
                     </select>
                 </div>
@@ -133,12 +53,11 @@
                     <label for="city_id">Город подачи</label>
                     <select name="city_id" id="city_id" required class="form-select">
                         @foreach ($citiesList as $city)
-                            <option value="{{ $city->id }}"
-                                    @if (old('city_id')) @if (old('city_id') == $city->id) selected @endif
-                                    @endif
-                                    @if ($ad->city_id == $city->id) selected @endif>
-                                {{ $city->name }}
-                            </option>
+                        <option value="{{ $city->id }}" @if (old('city_id')) @if (old('city_id')==$city->id) selected @endif
+                            @endif
+                            @if ($ad->city_id == $city->id) selected @endif>
+                            {{ $city->name }}
+                        </option>
                         @endforeach
                     </select>
                 </div>
@@ -146,48 +65,44 @@
                     <label for="status_id">Статус</label>
                     <select name="status_id" id="status_id" class="form-select">
                         @foreach ($statusesList as $status)
-                            <option value="{{ $status->id }}"
-                                    @if (old('status_id')) @if (old('status_id') == $status->id)
-                                    selected @endif
-                                    @endif
-                                    @if ($ad->status_id == $status->id) selected @endif>
-                                {{ $status->description }}
-                            </option>
+                        <option value="{{ $status->id }}" @if (old('status_id')) @if (old('status_id')==$status->id)
+                            selected @endif
+                            @endif
+                            @if ($ad->status_id == $status->id) selected @endif>
+                            {{ $status->description }}
+                        </option>
                         @endforeach
                     </select>
                 </div>
             </div>
-            </div>
-            <div class="container">
-                <div class="row mt-5">
-                    @forelse ($ad->images as $image)
-                        <div class="col-sm-4">
-                            <img src="{{ Storage::url($image->path) }}" alt="image" height=400>
-                            <div class="form-group d-flex justify-content-sm-evenly">
-                                <label for="imageMain">Сделать главной</label>
-                                <input type="radio" name="imageMain" id="imageMain" value="{{ $image->id }}"
-                                       class="form-check"
-                                       @if ($image->image_type == 0) checked @endif>
-                                <label for="">Удалить</label>
-                                <input type="checkbox" name="removeImage[]" id="removeImage" class="form-check"
-                                       value="{{ $image->id }}">
-                            </div>
-                        </div>
-                    @empty
-                        <div class="col-sm-6">
-                            <img src="{{ Storage::url('images/clean.webp') }}" alt="image" height=400>
-                        </div>
-                    @endforelse
+        </div>
+        <div class="container">
+            <div class="row mt-5">
+                @forelse ($ad->images as $image)
+                <div class="col-sm-4">
+                    <img src="{{ Storage::url($image->path) }}" alt="image" height=400>
+                    <div class="form-group d-flex justify-content-sm-evenly">
+                        <label for="imageMain">Сделать главной</label>
+                        <input type="radio" name="imageMain" id="imageMain" value="{{ $image->id }}" class="form-check" @if ($image->image_type == 0) checked @endif>
+                        <label for="">Удалить</label>
+                        <input type="checkbox" name="removeImage[]" id="removeImage" class="form-check" value="{{ $image->id }}">
+                    </div>
                 </div>
+                @empty
+                <div class="col-sm-6">
+                    <img src="{{ Storage::url('images/clean.webp') }}" alt="image" height=400>
+                </div>
+                @endforelse
             </div>
+        </div>
 
 
 
-            <div>
-                <button type="submit" class="btn btn-success">Сохранить</button>
-                <button type="reset" class="btn btn-danger">Отменить изменения</button>
->>>>>>> 6b1efc58bfd1c26931ac144dd62a7b129b91e88a
-            </div>
-        </form>
-    </div>
+        <div>
+            <button type="submit" class="btn btn-success">Сохранить</button>
+            <button type="reset" class="btn btn-danger">Отменить изменения</button>
+
+        </div>
+    </form>
+</div>
 @endsection
