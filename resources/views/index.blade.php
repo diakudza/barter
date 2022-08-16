d56d03a7e2f07ea916f875a07c9425995c92a532
Artem Biryukov <carbon0093@gmail.com>
2022-08-16 14:51:56 +0300

    feature: главный экран, хедер, футер

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -18,25 +18,7 @@
     </p>
   </div>
 
-
-  <!-- Блок поиска -->
-  <div id="search" class="d-flex flex-column align-items-center ">
-    <h3>Искать без регистрации!</h3>
-    @include('components.searchForm')
-  </div>
-
-  <!-- Последние 10 объявлений -->
-  <div class="d-flex flex-column mt-5 ">
-    <h3>Последние 10 обьявлений</h3>
-    <div class="products-last__list d-flex flex-row flex-wrap gap-2">
-      @foreach($lastTenAds as $item)
-      @include('components.adCart')
-      @endforeach()
-    </div>
-  </div>
-
 </div>
-
 
 <!-- Блок поиска -->
 <section id="main-search" class="container d-flex flex-row justify-content-sm-evenly">
