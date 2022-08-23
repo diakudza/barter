0b39a448da99769ef1e89c1ad8b6ddfb7f855b1f
diakudza <diakudza@gmail.com>
2022-08-23 14:18:07 +0300

    chat fix dir

--- resources/views/components/chats/chatMessageList.blade.php
+++ resources/views/components/chats/chatMessageList.blade.php
@@ -1,7 +1,7 @@
 <p>Сообщения </p>
 <div class="d-flex flex-column">
     @forelse($messages as $message)
-    @include('components.chatBubble')
+    @include('components.chats.chatBubble')
     @empty
     <p>пока нет сообщений!</p>
     @endforelse
