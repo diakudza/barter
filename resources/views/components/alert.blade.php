39a8107f4429440799d469d796d78fac3bee162c
Artem Biryukov <carbon0093@gmail.com>
2022-09-02 11:45:12 +0300

    feature: alert

--- resources/views/components/alert.blade.php
+++ resources/views/components/alert.blade.php
@@ -1,18 +1,18 @@
 @if (session('success'))
-<div class="alert alert-success">
+<div class="alert alert-success animate__animated">
     {{ session('success') }}
 </div>
 @endif
 
 @if (session('fail'))
-<div class="alert alert-danger">
+<div class="alert alert-danger animate__animated">
     {{ session('fail') }}
 </div>
 @endif
 
 @if ($errors->any())
 
-<div class="alert alert-danger">
+<div class="alert alert-danger animate__animated">
     <ul>
         @foreach ($errors->all() as $error)
         <li>{{ $error }}</li>
