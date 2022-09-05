afb52193bde03bfa72205ae14b6565478a314746
diakudza <diakudza@gmail.com>
2022-09-06 00:58:14 +0300

    Search fix

--- resources/views/search.blade.php
+++ resources/views/search.blade.php
@@ -10,18 +10,13 @@
 
     <section class="container search-result">
 
-        @if(!isset($searchResult))
-            <h3 class="search-result__title title">Давайте что-нибудь поищем!</h3>
-
-        @elseif(isset($searchResult))
+        @if(isset($searchResult))
             <h3 class="search-result__title title">Результаты поиска</h3>
 
             <div class="search-result__container">
 
                 @foreach ($searchResult as $item)
-
                     @include('components.cardsTemplate.littelCard')
-
                 @endforeach
 
             </div>
@@ -37,6 +32,8 @@
                     @endforeach
                 </div>
             </div>
+        @else
+            <h3 class="search-result__title title">Давайте что-нибудь поищем!</h3>
         @endif
 
     </section>
