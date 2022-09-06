7bd4d169fd00ea24e25f1e283559ea3cd073273a
Kirill <kin190173@gmail.com>
2022-09-06 18:33:20 +0500

    merge conflicts solved

--- resources/views/adShow.blade.php
+++ resources/views/adShow.blade.php
@@ -85,6 +85,7 @@
             <h2>Описание</h2>
             <p> {{ $ad['text'] }} </p>
         </div>
+        <a href="{{ route('complainAd', $ad->id) }}" class="link-danger fs-4">Пожаловаться на это объявление</a>
 
     </section>
 
