bd3f306604c9484552cad3adf9d0d704173671ab
diakudza <diakudza@gmail.com>
2022-09-08 20:49:20 +0300

    add bootstrap modal

--- resources/views/components/city.blade.php
+++ resources/views/components/city.blade.php
@@ -1,13 +1,31 @@
 <div>
-    <div> {{$userCity}} </div>
-    <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal" data-bs-whatever="@mdo">Open modal for @mdo</button>
-    <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-        <div>
-        @foreach($cities as $city)
-            <div id="{{$city->id}}"> {{$city->name}}</div>
-        @endforeach
+    <button type="button" class="btn" data-bs-toggle="modal" data-bs-target="#staticBackdrop">
+        Ваш город - {{$userCity}}
+    </button>
+
+    <div class="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="staticBackdropLabel">Modal title</h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body ">
+                                        <div class="overflow-scroll">
+                                            @foreach($cities as $city)
+                                                <div id="{{$city->id}}"> {{$city->name}}</div>
+                                            @endforeach
+                                        </div>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                    <button type="button" class="btn btn-primary">Understood</button>
+                </div>
+            </div>
         </div>
     </div>
+
+
 
     @if(session()->has('showCityChoice') && session('showCityChoice')))
     <div>
