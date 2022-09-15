06865e270a0f8520d22b2c471a2bcf8840ce56a6
pc <no-mail>
2022-09-15 20:55:05 +0300

    admin-nav-update-2

--- resources/views/admin/users.blade.php
+++ resources/views/admin/users.blade.php
@@ -6,7 +6,7 @@
 
     <section class="container mt-5 pt-5">
 
-        <form class="formFilter" action="{{ route('user.index') }}" method="get">
+        <form class="formFilter form-sort__header" action="{{ route('user.index') }}" method="get">
             @csrf
 
             <h2>Фильтровать пользователей по:</h2>
@@ -44,7 +44,6 @@
                     <label for="status1">Активен</label>
                     <input type="checkbox" name="status[]" id="status1" value="1"
                         @if (isset($filterStatuses) && in_array(1, $filterStatuses)) checked @endif>
-
                 </div>
                 <div>
                     <label for="status2">Заблокирован</label>
@@ -76,21 +75,22 @@
                 </div>
             </div>
 
-            <div class="form-control">
+            <div class="form-control d-flex form-sort">
                 <label for="user">Имя, e-mail</label>
                 <input type="text" name="user" id="user"
                     @if (isset($searchString)) value="{{ $searchString }}" @endif>
             </div>
-            <button type="submit" class="btn btn-danger bthChange">
+
+            <button type="submit" class="btn btn-danger admin-table__bth-change">
             <p>Применить фильтр</p>
             </button>
-            <a href="{{ route('user.index') }}" class="btn btn-secondary bthChange">
+            <a href="{{ route('user.index') }}" class="btn btn-secondary admin-table__bth-change">
                 <p>Сбросить фильтры</p>
             </a>
         </form>
 
-        <table class="table mt-5 w-100 table-bordered">
-            <thead>
+        <table class="table mt-5 w-100 table-bordered admin-table">
+            <thead class="admin-table__header">
                 <td>id</td>
                 <td>Имя</td>
                 <td>Почта</td>
@@ -105,7 +105,7 @@
                     <td><a href="{{ route('user.show', $user['id']) }}"> {{ $user['name'] }}</a> </td>
                     <td> {{ $user['email'] }} </td>
                     <td>
-                        <form class="form-group" action="{{ route('user.update', $user['id']) }}" method="post">
+                        <form class="form-group admin-table__column" action="{{ route('user.update', $user['id']) }}" method="post">
                             @csrf
                             @method('PUT')
                             <select name="role_id" class="form-select">
@@ -114,7 +114,7 @@
                                         {{ $role['role'] }}</option>
                                 @endforeach
                             </select>
-                            <button class="btn btn-success bthChange" type="submit">
+                            <button class="btn btn-success bthChange admin-table__bth-change" type="submit">
                                 <p>Изменить</p>
                                 <svg class="svgAdmin" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" xmlns:svgjs="http://svgjs.com/svgjs" version="1.1" width="512" height="512" x="0" y="0" viewBox="0 0 24 24" style="enable-background:new 0 0 512 512" xml:space="preserve"><g><path xmlns="http://www.w3.org/2000/svg" d="M18.656.93,6.464,13.122A4.966,4.966,0,0,0,5,16.657V18a1,1,0,0,0,1,1H7.343a4.966,4.966,0,0,0,3.535-1.464L23.07,5.344a3.125,3.125,0,0,0,0-4.414A3.194,3.194,0,0,0,18.656.93Zm3,3L9.464,16.122A3.02,3.02,0,0,1,7.343,17H7v-.343a3.02,3.02,0,0,1,.878-2.121L20.07,2.344a1.148,1.148,0,0,1,1.586,0A1.123,1.123,0,0,1,21.656,3.93Z" fill="#e5e9ec" data-original="#000000"/><path xmlns="http://www.w3.org/2000/svg" d="M23,8.979a1,1,0,0,0-1,1V15H18a3,3,0,0,0-3,3v4H5a3,3,0,0,1-3-3V5A3,3,0,0,1,5,2h9.042a1,1,0,0,0,0-2H5A5.006,5.006,0,0,0,0,5V19a5.006,5.006,0,0,0,5,5H16.343a4.968,4.968,0,0,0,3.536-1.464l2.656-2.658A4.968,4.968,0,0,0,24,16.343V9.979A1,1,0,0,0,23,8.979ZM18.465,21.122a2.975,2.975,0,0,1-1.465.8V18a1,1,0,0,1,1-1h3.925a3.016,3.016,0,0,1-.8,1.464Z" fill="#e5e9ec" data-original="#000000"/></g>
                                 </svg>
@@ -123,7 +123,7 @@
                     </td>
 
                     <td>
-                        <form class="form-group" action="{{ route('user.update', $user['id']) }}" method="post">
+                        <form class="form-group admin-table__column" action="{{ route('user.update', $user['id']) }}" method="post">
                             @csrf
                             @method('PUT')
                             <select name="status_id" class="form-select">
@@ -132,7 +132,7 @@
                                         {{ $status['status'] }}</option>
                                 @endforeach
                             </select>
-                            <button class="btn btn-success bthChange" type="submit">
+                            <button class="btn btn-success bthChange admin-table__bth-change" type="submit">
                                 <p>Изменить</p>
                                 <svg class="svgAdmin" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" xmlns:svgjs="http://svgjs.com/svgjs" version="1.1" width="512" height="512" x="0" y="0" viewBox="0 0 24 24" style="enable-background:new 0 0 512 512" xml:space="preserve"><g><path xmlns="http://www.w3.org/2000/svg" d="M18.656.93,6.464,13.122A4.966,4.966,0,0,0,5,16.657V18a1,1,0,0,0,1,1H7.343a4.966,4.966,0,0,0,3.535-1.464L23.07,5.344a3.125,3.125,0,0,0,0-4.414A3.194,3.194,0,0,0,18.656.93Zm3,3L9.464,16.122A3.02,3.02,0,0,1,7.343,17H7v-.343a3.02,3.02,0,0,1,.878-2.121L20.07,2.344a1.148,1.148,0,0,1,1.586,0A1.123,1.123,0,0,1,21.656,3.93Z" fill="#e5e9ec" data-original="#000000"/><path xmlns="http://www.w3.org/2000/svg" d="M23,8.979a1,1,0,0,0-1,1V15H18a3,3,0,0,0-3,3v4H5a3,3,0,0,1-3-3V5A3,3,0,0,1,5,2h9.042a1,1,0,0,0,0-2H5A5.006,5.006,0,0,0,0,5V19a5.006,5.006,0,0,0,5,5H16.343a4.968,4.968,0,0,0,3.536-1.464l2.656-2.658A4.968,4.968,0,0,0,24,16.343V9.979A1,1,0,0,0,23,8.979ZM18.465,21.122a2.975,2.975,0,0,1-1.465.8V18a1,1,0,0,1,1-1h3.925a3.016,3.016,0,0,1-.8,1.464Z" fill="#e5e9ec" data-original="#000000"/></g>
                                 </svg>
@@ -140,12 +140,17 @@
                         </form>
                     </td>
 
-                    <td class="d-flex flex-row">
+                    <td class="admin-table__column-del d-flex flex-row">
                         <form action="{{ route('user.destroy', $user['id']) }}" method="post">
                             @method('DELETE')
                             @csrf
-                            <Button class="btn btn-danger bthDel">
-                                <svg class="svgAdmin" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" xmlns:svgjs="http://svgjs.com/svgjs" version="1.1" width="512" height="512" x="0" y="0" viewBox="0 0 24 24" style="enable-background:new 0 0 512 512" xml:space="preserve"><g><path xmlns="http://www.w3.org/2000/svg" d="M21,4H17.9A5.009,5.009,0,0,0,13,0H11A5.009,5.009,0,0,0,6.1,4H3A1,1,0,0,0,3,6H4V19a5.006,5.006,0,0,0,5,5h6a5.006,5.006,0,0,0,5-5V6h1a1,1,0,0,0,0-2ZM11,2h2a3.006,3.006,0,0,1,2.829,2H8.171A3.006,3.006,0,0,1,11,2Zm7,17a3,3,0,0,1-3,3H9a3,3,0,0,1-3-3V6H18Z" fill="#f9fcff" data-original="#000000"/><path xmlns="http://www.w3.org/2000/svg" d="M10,18a1,1,0,0,0,1-1V11a1,1,0,0,0-2,0v6A1,1,0,0,0,10,18Z" fill="#f9fcff" data-original="#000000"/><path xmlns="http://www.w3.org/2000/svg" d="M14,18a1,1,0,0,0,1-1V11a1,1,0,0,0-2,0v6A1,1,0,0,0,14,18Z" fill="#f9fcff" data-original="#000000"/></g></svg>
+                            <Button class="btn btn-danger bthDel admin-table__bth-delete">
+                                <svg class="admin-table__bth-svg" width="48" height="49" viewBox="0 0 48 49" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                    <path d="M30.1042 22.3906C30.1042 22.3906 29.6517 28.0031 29.3892 30.3673C29.2642 31.4965 28.5667 32.1581 27.4242 32.179C25.25 32.2181 23.0733 32.2206 20.9 32.1748C19.8008 32.1523 19.115 31.4823 18.9925 30.3731C18.7283 27.9881 18.2783 22.3906 18.2783 22.3906" stroke="#EF4646" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+                                    <path d="M31.2567 19.6999H17.125" stroke="#EF4646" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+                                    <path d="M28.5335 19.7C27.8793 19.7 27.316 19.2375 27.1877 18.5966L26.9852 17.5833C26.8602 17.1158 26.4368 16.7925 25.9543 16.7925H22.4268C21.9443 16.7925 21.521 17.1158 21.396 17.5833L21.1935 18.5966C21.0652 19.2375 20.5018 19.7 19.8477 19.7" stroke="#EF4646" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+                                    <rect x="1" y="1.5" width="46" height="46" rx="23" stroke="#EF4646" stroke-width="2"/>
+                                </svg>
                             </Button>
                         </form>
                     </td>
