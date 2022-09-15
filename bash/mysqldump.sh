377f561ee300afe8a5abd7f2ff510009e542c0e7
diakudza <diakudza@gmail.com>
2022-09-15 22:00:02 +0300

    make dump fix

--- bash/mysqldump.sh
+++ bash/mysqldump.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-/usr/bin/mysqldump --complete-insert --extended-insert --skip-add-locks --disable-keys --add-drop-table --skip-lock-tables --user=root --host=127.0.0.1 > storage/app/public/backup/dump.sql
+/usr/bin/mysqldump --complete-insert --extended-insert --skip-add-locks --disable-keys --add-drop-table --skip-lock-tables --user=root --password=password --host=mysql barter > storage/app/public/backup/dump.sql
 
 echo "mysqldump complete"
