231737695449f84245d831f4b57bbc4a56593dc4
diakudza <diakudza@gmail.com>
2022-09-20 14:40:20 +0300

    don't show deleted ads

--- app/Queries/QueryBuilderAds.php
+++ app/Queries/QueryBuilderAds.php
@@ -18,7 +18,11 @@ class QueryBuilderAds extends QueryBuilderBase implements QueryBuilder
 
     public function listAdsByUser(int $userId): LengthAwarePaginator
     {
-        return $this->model::where('user_id', $userId)->whereRelation('status', 'title', '!=', 'deleted')->with(['city', 'category', 'status', 'imageMain'])->paginate(15);
+        return $this->model::where('user_id', $userId)
+
+            ->whereRelation('status', 'title', '=', 'active')
+            ->orWhereRelation('status', 'title', '!=', 'deleted')
+            ->with(['city', 'category', 'status', 'imageMain'])->paginate(15);
     }
 
     public function getAdDetailById(int $adId): Ad
