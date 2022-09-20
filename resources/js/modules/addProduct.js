da33fa75a4e4fcc9aaeed8c78deade8a829996ed
Artem Biryukov <carbon0093@gmail.com>
2022-09-20 12:35:01 +0300

    перед страницей польователя

--- resources/js/modules/addProduct.js
+++ resources/js/modules/addProduct.js
@@ -18,32 +18,32 @@ const addProducts = () => {
     })
 
     photoInput.addEventListener("change", function (e) {
-        let filesItems = e.target.files[0];
+        let filesItems = e.target.files;
+        let itemFile;
 
-        let picture = new FileReader();
+        // let picture = new FileReader();
+        //
+        // picture.readAsDataURL(filesItems);
+        // picture.addEventListener('load', function (event) {
+        //     document.getElementById('uploadedImage').setAttribute('src', event.target.result);
+        // });
 
-        picture.readAsDataURL(filesItems);
-        picture.addEventListener('load', function (event) {
-            document.getElementById('uploadedImage').setAttribute('src', event.target.result);
-        });
 
-
-        // if (filesItems.length > 1) {
-        //     for (let i = 0; i < filesItems.length; i++) {
-        //         itemFile = `
-        //             <div class="load-file__item">
-        //                 <h4 class="load-file__item-name">Название: ${filesItems[i].name}</h4>
-        //             </div>`;
-        //         // loadFileWrapper.insertAdjacentHTML('afterbegin', itemsHtml);
-        //     }
-        // } else {
-        //     itemFile = `
-        //         <div class="load-file__item">
-        //             <h4 class="load-file__item-name">Название: ${filesItems[0].name}</h4>
-        //         </div>`;
-        //     loadFileWrapper.innerHTML = itemFile;
-        // }
-
+        if (filesItems.length > 1) {
+            for (let i = 0; i < filesItems.length; i++) {
+                itemFile = `
+                    <div class="load-file__item">
+                        <h4 class="load-file__item-name">Название: ${filesItems[i].name}</h4>
+                    </div>`;
+                // loadFileWrapper.insertAdjacentHTML('afterbegin', itemsHtml);
+            }
+        } else {
+            itemFile = `
+                <div class="load-file__item">
+                    <h4 class="load-file__item-name">Название: ${filesItems[0].name}</h4>
+                </div>`;
+            loadFileWrapper.innerHTML = itemFile;
+        }
 
     });
 
